3cdae07667da7ca71e706990b23ab8dd8dc7379f
Ilya Pekelny <ipekelny@mirantis.com>
2014-02-03 18:04:00 +0200

    delete

--- bt.c
+++ bt.c
@@ -10,6 +10,9 @@ struct node {
 };
 
 struct node *root = 0;
+struct node *child;
+
+int tmp_right;
 
 void destroy_tree(struct node *leaf) {
   if (leaf != 0) {
@@ -34,6 +37,31 @@ insert(int key, struct node **leaf) {
   }
 }
 
+delete(int key, struct node *leaf) {
+  if (leaf != 0){
+    if (*leaf == key) {
+      if (*leaf->right != 0) {
+
+        *child = leaf->right;
+        while (child->left != 0)
+          *child = *leaf->left;
+        *leaf->key_value = *child->key_value;
+        if (*child->right) {
+          insert(*child->right, *leaf->right);
+        }
+
+      }
+    }
+    else if (key < *leaf) {
+      return delete(key, *leaf->left);
+    }
+    else if (key > *leaf) {
+      return delete(key, *leaf->right);
+    }
+  }
+  return 0;
+}
+
 struct node *search(int key, struct node *leaf) {
   if (leaf != 0) {
     if(key == leaf->key_value) {
@@ -51,7 +79,6 @@ struct node *search(int key, struct node *leaf) {
 
 main() {
   char action, line[1024], *e;
-  const char *al_actions = "idl";
   int value;
 
   while ((action = getchar()) && fgets(line, sizeof(line), stdin)) {
@@ -64,6 +91,9 @@ main() {
         break;
       case 'l':
         printf("l %lu\n", strtol(line, &e, 10));
+        break;
+      case 'a':
+        printf("d %lu\n", strtol(line, &e, 10));
         break;
       default:
         exit(0);
