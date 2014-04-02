48e6f15c460648399c0e9f1bf6468103e1505a92
Ilya Pekelny <ipekelny@mirantis.com>
2014-04-02 19:57:56 +0300

    New node! Hell Yeahhh!!!

--- B_tree.c
+++ B_tree.c
@@ -7,25 +7,27 @@ struct key {
   struct node* right;
 };
 
-struct key* entry[(T_FUCTOR * 2)-1];
+struct key* entry = NULL;
 
 struct key *NewNode(int* value) {
-  struct key** new_node;
   struct key* new_key;
-
-  new_node = (struct key*)malloc(sizeof(new_key));
-  new_key  = malloc(sizeof(new_key));
+  struct key* new_node = malloc(sizeof *new_key);
 
   new_key->data = value;
   new_key->left = NULL;
   new_key->right = NULL;
 
-  new_node[0] = new_key;
+  new_node[0] = *new_key;
 
-  return new_node[0];
+  return new_node;
 }
 
-struct node *insert(struct node* entry, int* value) {
+struct node *insert(struct key* entry, int* value) {
+  if (entry == NULL)
+    entry = NewNode(value);
+  else {
+    // Add new key to the tree through the entry point.
+  }
 }
 
 struct node *lookup(struct node* entry, int* value) {
