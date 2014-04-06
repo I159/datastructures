5ce5265f7d492ab2df0872b51add5f01482cc026
Ilya Pekelny <ipekelny@mirantis.com>
2014-04-06 11:53:35 +0300

    Flat insert in the entry

--- B_tree.c
+++ B_tree.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #define T_FUCTOR 3
+#define MAX_DEGREE (T_FUCTOR * 2)
 
 struct key {
   int* data;
@@ -8,6 +9,9 @@ struct key {
 };
 
 struct key* entry = NULL;
+struct key* current = NULL;
+int *length;
+int *tmp;
 
 struct key *NewNode(int* value) {
   struct key* new_key;
@@ -25,8 +29,12 @@ struct key *NewNode(int* value) {
 struct node *insert(struct key* entry, int* value) {
   if (entry == NULL)
     entry = NewNode(value);
-  else {
+  else if ((length = sizeof(entry)/sizeof(entry[0])) < *MAX_DEGREE){
     // Add new key to the tree through the entry point.
+    entry = realloc(entry, *(length + 1) * sizeof(entry[0]));
+    for (length; (*entry[length] > value) || (*length >= 0); length--)
+      entry[length + 1] = entry[length];
+    entry[length] = value;
   }
 }
 
